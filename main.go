66ab4f39244175d1ce783e7d295981f3394c97ff
AlexanderOkhrimenko <xanderhard@icloud.com>
2019-11-10 12:25:00 +0300

    Add Graph

--- main.go
+++ main.go
@@ -11,17 +11,17 @@ import (
 func main() {
 
 	// ---- Binary Sort
-	var x = MakeArray(10) 				// new array of 10 zeros
-	fmt.Println(x) 							// viewing an empty array
-	RandomFillArray(x, 100) 	// filling an array with random numbers
-	fmt.Println(x) 							// viewing an empty array
-	sort.Ints(x)							// sorting array
-	fmt.Println(x)							// viewing an sorting array
-	fmt.Println(BinarySearch(x,50)) 	// search index "50" in array
+	var x = MakeArray(10)            // new array of 10 zeros
+	fmt.Println(x)                   // viewing an empty array
+	RandomFillArray(x, 100)          // filling an array with random numbers
+	fmt.Println(x)                   // viewing an empty array
+	sort.Ints(x)                     // sorting array
+	fmt.Println(x)                   // viewing an sorting array
+	fmt.Println(BinarySearch(x, 50)) // search index "50" in array
 
 	// ---- Select Sort
 	var x1 = MakeArray(20)
-	RandomFillArray(x1,1000)
+	RandomFillArray(x1, 1000)
 	fmt.Println(x1)
 	fmt.Println(SelectorSort(x1))
 
@@ -33,15 +33,15 @@ func main() {
 
 	//------- Quick Sort
 	var x2 = MakeArray(10)
-	RandomFillArray(x2,1000)
+	RandomFillArray(x2, 1000)
 	fmt.Println(x2)
 	fmt.Println(QuickSort(x2))
 
-	//----- Hash ------
+	//----- Hash -------------
 	m := make(map[string]int)
 	m1 := new(map[string]int)
 	var m2 map[string]int
-	m3 := map[string]int{"apple":99, "orange":100}
+	m3 := map[string]int{"apple": 99, "orange": 100}
 
 	// Insert data
 	m3["carrot"] = 101
@@ -50,49 +50,71 @@ func main() {
 	delete(m3, "apple")
 
 	// Search data
-	xx , aa := m3["orange"]
-	fmt.Println("target value" ,xx ,aa)
+	xx, aa := m3["orange"]
+	fmt.Println("target value", xx, aa)
 
-	//
+	fmt.Println(m, m1, m2, m3)
+	//-------------------------
 
+	// ---- Graph ---------------
+	graph := make(map[string][]string)
+	c := make(chan string, 300)
 
-	fmt.Println(m,m1,m2,m3)
+	graph["you"] = []string{"video", "audio", "tv"}
+	graph["pat"] = []string{"js", "php", "nodajs"}
+	graph["danya"] = []string{"qa", "test", "graph"}
+	graph["max"] = []string{}
+
+	fmt.Println("Test Graph", graph)
+	// Pipe
+	c <- graph["you"][0]
+	fmt.Println(<-c)
+
+	//---------------------------
 }
 
-											// creating an empty array
-func MakeArray (size int) []int {
-	x := make([]int,size)
+// creating an empty array
+func MakeArray(size int) []int {
+	x := make([]int, size)
 	return x
 }
-											// Filling an array with random numbers
-func RandomFillArray (arr []int , rangeRandom int) []int {
+
+// Filling an array with random numbers
+func RandomFillArray(arr []int, rangeRandom int) []int {
 
 	rand.Seed(time.Now().Unix())
-	for i := 0 ; i < len(arr); i++  {
+	for i := 0; i < len(arr); i++ {
 		arr[i] = rand.Intn(rangeRandom)
 	}
 	return arr
 }
 
-func BinarySearch (arr []int, item int) int {
+func BinarySearch(arr []int, item int) int {
 
-	low := 0 								// lower bound of range
-	high := len(arr) - 1 					// height bound of range
+	low := 0             // lower bound of range
+	high := len(arr) - 1 // height bound of range
 
 	for low <= high {
-		mid := (low + high) / 2				// reduce the search range
+		mid := (low + high) / 2 // reduce the search range
 		guess := arr[mid]
-		if guess == item { return mid }
-		if guess > item { high = mid - 1 } else { low = mid + 1 }
+		if guess == item {
+			return mid
+		}
+		if guess > item {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
 	}
 	return -1
 }
-											// Selection sort
-func SelectorSort (arr []int) []int {
 
-	for i := 0 ; i < len(arr) -1 ; i++ {
-		var min  = i
-		for j := i + 1 ; j < len(arr); j++ {
+// Selection sort
+func SelectorSort(arr []int) []int {
+
+	for i := 0; i < len(arr)-1; i++ {
+		var min = i
+		for j := i + 1; j < len(arr); j++ {
 			if arr[j] < arr[min] {
 				min = j
 			}
@@ -104,12 +126,12 @@ func SelectorSort (arr []int) []int {
 	return arr
 }
 
-func Recursive ( i int) {
+func Recursive(i int) {
 	fmt.Println(i)
 	if i <= 0 {
 		return
 	} else {
-		Recursive(i-1)
+		Recursive(i - 1)
 	}
 }
 
@@ -117,11 +139,11 @@ func Fact(x int) int {
 	if x == 1 {
 		return 1
 	} else {
-		return  x * Fact(x-1)
+		return x * Fact(x-1)
 	}
 }
 
-func QuickSort (arr []int) []int{
+func QuickSort(arr []int) []int {
 
 	if len(arr) < 2 {
 		return arr
@@ -145,4 +167,3 @@ func QuickSort (arr []int) []int{
 
 	return arr
 }
-
