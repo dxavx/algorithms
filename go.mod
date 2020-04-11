1347323a0054b39de092cccce1b8832a65e80694
Охрименко Александр <a.ohrimenko@id-network.ru>
2020-04-11 22:50:45 +0300

    Add RandomFillArray

---
+++ go.mod
@@ -0,0 +1,5 @@
+module algorithms_search
+
+go 1.14
+
+require github.com/stretchr/testify v1.5.1
--- main_test.go
+++ main_test.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMakeArray(t *testing.T) {
+	n := 10
+	nm := MakeArray(n)
+	assert.Equal(t, n, len(nm))
+}
+
+func TestRandomFillArray(t *testing.T) {
+	value := false
+	nm := MakeArray(10)
+	RandomFillArray(nm, 100)
+	if nm[0] != nm[1] {
+		value = true
+	}
+
+	assert.True(t, value)
+}
